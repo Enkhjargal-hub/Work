e90326c3cd19e0d06f75480edd93b76e36db02ca
Enkhjargal-hub <Joloenja@gmail.com>
2025-02-18 10:27:08 +0800

    j

---
+++ src/routes/index.js
@@ -0,0 +1,36 @@
+// const express = require("express"); // express module import hiigd express.js tataj avch bn
+// const app = express(); // app huvisagch ni express hadgalj bn
+// const port = 3000;
+
+// app.use(express.json()); //ogogdol unshihad tusalj bga chuhal
+
+// app.post("/api", (request, response) => { //api zam dr post huselt huleej avna
+//     response.json({
+//         name: "Jolo",
+//         email: "joloenja@gmail.com",
+//         password: "94126297"
+//     });
+// }); 
+
+// app.get("/users")
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+
+import express from 'express';
+import { useRouter } from './routes/users-Router.js';
+import { postRouter } from './routes/ post-router.js';
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.use('/', useRouter)
+app.use('/post', postRouter)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);   
+})
